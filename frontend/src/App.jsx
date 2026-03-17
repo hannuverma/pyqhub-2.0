a92e3460435f49f65328cfd4183bf6fbc96e8865
shivanshu <shivanshumangal007@gmail.com>
2026-03-17 22:17:49 +0530

    animation added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Home from './components/Home'
 import Main from './components/Main'
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 const App = () => {
   return (
     <>
